77af02b2d2029969a2d54295eea455dca9c31980
pierre8181 <prabuddhaatul@gmail.com>
2025-01-05 16:47:29 -0800

    show image

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def annotate_image(image, detections):
     output_image = MASK_ANNOTATOR.annotate(output_image, detections)
     output_image = BOX_ANNOTATOR.annotate(output_image, detections)
     output_image = LABEL_ANNOTATOR.annotate(output_image, detections)
-    print(output_image)
+    output_image.save("output.png")
     return output_image
 
 
